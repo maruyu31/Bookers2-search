58dfe6bcaa75e84c57fc49ee05ee63ecd9384c86
maruyu31 <you3746336@gmail.com>
2021-06-02 08:22:44 +0000

    [Add] モデルにアソシエーションを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
 end
