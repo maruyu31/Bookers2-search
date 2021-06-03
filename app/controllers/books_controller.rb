b795f6db22ca35e5ced9c6ea3c0b2f2d07f56163
maruyu31 <you3746336@gmail.com>
2021-06-03 06:00:17 +0000

    [Add] bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,35 @@
 class BooksController < ApplicationController
   def index
-    
+    @book = Book.new
+    @books = Book.all
   end
-  
+
   def show
-    
+
   end
-  
+
   def edit
-    
+
   end
-  
+
   def create
-    
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to user_path(current_user.id)
   end
-  
+
   def update
-    
+
   end
-  
+
   def destroy
-    
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
