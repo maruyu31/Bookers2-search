5158a615245fdd366d39591992401b86c8420a51
maruyu31 <you3746336@gmail.com>
2021-06-04 01:21:51 +0000

    [Add] userのshowページ、indexページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
-    
+    @users = User.all
+    @newbook = Book.new
   end
   
   def show
-    
+    @user = User.find(params[:id])
+    @books = @user.books
   end
   
   def edit
