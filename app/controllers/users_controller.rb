3e277fc64b2d41ff12b48d75ab579ac923e3a3f9
maruyu31 <you3746336@gmail.com>
2021-06-04 03:58:23 +0000

    [Add] ユーザーのupdate機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,25 @@ class UsersController < ApplicationController
     @users = User.all
     @newbook = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @newbook = Book.new
   end
-  
+
   def edit
-    
+    @user = User.find(params[:id])
   end
-  
+
   def update
-    
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7e48a132ea1405c7876c2d4d2c551caf998a17ec6593541a3a2dfbd5acf29e26b6d8c0762ad23c27c10d2fa1bbdffb453b5174d478dfb1c9b37cd8529df7aac9'
