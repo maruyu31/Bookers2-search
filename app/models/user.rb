0db2027ad78b05c497e6ee24a77b550eace5c05a
maruyu31 <you3746336@gmail.com>
2021-07-03 08:43:40 +0000

    [Add] follow機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,26 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follows, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
+
+  def follow(other_user)
+    follows << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def follows?(other_user)
+    follows.include?(other_user)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => "homes#top"
   get "home/about" => "homes#about", as: "about"
-  resources :users, only:[:index, :show, :edit, :update]
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+  end
   resources :books, only:[:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
 end
